02d71a8dcb31de87774b6f8f1d3ab161d5235472
alex sandro <alexsandropimenta16@gmail.com>
2025-06-11 11:58:49 -0300

    kioficina

--- app/views/template/head.php
+++ app/views/template/head.php
@@ -5,7 +5,11 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="theme-color" content="#ffc107">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?php echo BASE_URL ?>assets/img/logo-preto.png" type="image/png">
     <link rel="stylesheet" href="<?php echo BASE_URL ?>assets/css/reset.css">
     <link rel="stylesheet" href="<?php echo BASE_URL ?>assets/css/style.css">
+
     <title><?php echo $titulo; ?></title>
+    <link rel="manifest" href="manifest.json">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css?v=<?= time()?>">
 </head>
